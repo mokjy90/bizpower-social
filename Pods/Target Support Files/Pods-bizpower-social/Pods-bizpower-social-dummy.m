53487799e8e544b09195962d87191c8a06f992d2
Apple <you@example.com>
2017-01-09 17:22:48 +0800

    Completed setting up firebase

---
+++ Pods/Target Support Files/Pods-bizpower-social/Pods-bizpower-social-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bizpower_social : NSObject
+@end
+@implementation PodsDummy_Pods_bizpower_social
+@end
---
+++ bizpower-social/SigninVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bizpower-social
+//
+//  Created by Apple on 7/1/17.
+//  Copyright © 2017 Mjy. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
